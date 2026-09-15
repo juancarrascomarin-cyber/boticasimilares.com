9dea67b80ad17220e4abb52ec9852f5c87f78b1e
juancarrascomarin-cyber <juan_carrasco_marin@boticasimilares.com>
2026-09-15 14:00:50 -0500

    Preparar integración futura con redes sociales

--- integrations.config.js
+++ integrations.config.js
@@ -13,5 +13,30 @@ window.BOTICAS_INTEGRATIONS = {
     widgetId: "",
     position: "bottom-right",
     fallbackWhatsApp: "https://wa.me/51990993247?text=Hola%2C%20necesito%20ayuda"
+  },
+  social: {
+    enabled: false,
+    facebook: "",
+    instagram: "",
+    tiktok: "",
+    youtube: "",
+    linkedin: "",
+    x: "",
+    threads: "",
+    whatsappChannel: ""
+  },
+  socialCommerce: {
+    enabled: false,
+    facebookShopUrl: "",
+    instagramShopUrl: "",
+    tiktokShopUrl: "",
+    campaignLandingBaseUrl: ""
+  },
+  tracking: {
+    enabled: false,
+    metaPixelId: "",
+    tiktokPixelId: "",
+    googleTagId: "",
+    consentRequired: true
   }
 };
